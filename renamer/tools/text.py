f6c29c12ac85e61f5a446d0373a42cbeb3831b29
itsmeminion <88274133+itsmeminion@users.noreply.github.com>
2021-08-14 14:55:18 +0530

    Add files via upload

---
+++ renamer/tools/text.py
@@ -0,0 +1,54 @@
+from pyrogram.emoji import *
+
+class TEXT:
+    DOWNLOAD_START = f"Downloading Starts soon {SLEEPING_FACE}"
+    UPLOAD_START = f"Upload Starting Soon {SLEEPING_FACE}"
+    UPLOAD_SUCESS = f"Thanks for using [me](https://github.com/CB_RenamerBot)"
+    BANNED_USER_TEXT = f"Hey bro, you are **banned** from using me {FACE_WITH_TEARS_OF_JOY}."
+    NOT_LOGGED_TEXT = f"This bot was only for private use {LOCKED_WITH_KEY}. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
+    SAVED_CUSTOM_THUMBNAIL = f"Thumbnail Saved Permanently {NOTEBOOK_WITH_DECORATIVE_COVER}"
+    DELETED_CUSTOM_THUMBNAIL = f"Thumbnail Deleted Successfully {CHECK_MARK_BUTTON}"
+    NO_CUSTOM_THUMB_NAIL_FOUND = f"𝖭𝗈 𝗍𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅 𝖥𝗈𝗎𝗇𝖽 {THUMBS_DOWN_LIGHT_SKIN_TONE}"
+    THUMBNAIL_CAPTION = f"{BACKHAND_INDEX_POINTING_UP_LIGHT_SKIN_TONE} Your Permanent thumbnail"
+
+
+    ABOUT = """**𝖬𝗒 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 :**
+
+** My Name:** {bot_name}
+    
+** Language:** [Python 3](https://www.python.org/)
+
+** FrameWork:** [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+** Developer:** [CineBin](https://t.me/CineBin)
+
+** Channel:** [CB BOTS](https://t.me/CB_Bots)
+
+** Group:** [CB BOT SUPPORT](https://t.me/CB_Bots/10)
+
+** Source Code:** [Press Me](https://github.com)
+"""
+
+    HELP_USER = """**Follow Below Steps:**
+   
+☞︎︎︎ Use /mode command to change upload mode.
+☞︎︎︎ Send a photo to set as permanent thumbnail.
+☞︎︎︎ Now send me the Telegram file you want to rename.
+☞︎︎︎ Send the new name when bot ask.
+
+For source code check about
+"""
+
+    START_TEXT = """Hi {user_mention},
+
+I am a telegram renamer with permanent thumbnail.
+
+𝖥𝗈𝗋 𝖬𝗈𝗋𝖾 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 𝖲𝖾𝖾 𝖧𝖾𝗅𝗉.
+
+**Maintained By:** [CB BOTS](https://t.me/CB_Bots)
+"""
+
+
+    DONATE_USER = """**__Thanks for showing interest in donation.__**
+ 
+But We Give Our Service Free For U
